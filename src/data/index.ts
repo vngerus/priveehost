cac41320058d9f0311fcf869124e4d85e137b192
angel <angelsmithlgs@gmail.com>
2024-11-05 11:45:07 -0300

    feat: icons y data add

---
+++ src/data/index.ts
@@ -0,0 +1,6 @@
+export const navItems = [
+    { link: "Overview", path: "home" },
+    { link: "Feature", path: "feature" },
+    { link: "About", path: "about" },
+    { link: "Pricing", path: "pricing" },
+  ];
